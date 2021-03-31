2842643bcd4456f79835a3acf42f6d07b78b8462
Avro504 <30928091+Avro504@users.noreply.github.com>
2021-03-31 01:11:51 +0100

    ONS Weeklies year tweak

---
+++ SQL Scripts/ONS_Weekly_insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO `Covid-19`.`ONS_Weeklies`(`year`, `week_number`, `week_ended`, `uk`, `england`, `wales`, `scotland`, `northern_ireland`)  
+VALUES (2021, 11, "2021-03-19", 1043, 912, 49, 65, 15);
